6712acce7694f085055dab45a893899b7bb8ddcf
git config --global user.name <nyaribovic@gmail.com>
2023-07-26 04:31:14 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,76 +1,13 @@
-/*
+i/*
  * File: _printf.c
  * Auth: Victor Bundi,
  *       Chepngetich Brenda.
  *      
  */
 
+#include <unistd.h>
+#include <stdarg.h>
 #include "main.h"
-
-void cleanup(va_list args, buffer_t *output);
-int run_printf(const char *format, va_list args, buffer_t *output);
-int _printf(const char *format, ...);
-
-/**
- * cleanup - Peforms cleanup operations for _printf.
- * @args: A va_list of arguments provided to _printf.
- * @output: A buffer_t struct.
- */
-void cleanup(va_list args, buffer_t *output)
-{
-	va_end(args);
-	write(1, output->start, output->len);
-	free_buffer(output);
-}
-
-/**
- * run_printf - Reads through the format string for _printf.
- * @format: Character string to print - may contain directives.
- * @output: A buffer_t struct containing a buffer.
- * @args: A va_list of arguments.
- *
- * Return: The number of characters stored to output.
- */
-int run_printf(const char *format, va_list args, buffer_t *output)
-{
-	int i, wid, prec, ret = 0;
-	char tmp;
-	unsigned char flags, len;
-	unsigned int (*f)(va_list, buffer_t *,
-			unsigned char, int, int, unsigned char);
-
-	for (i = 0; *(format + i); i++)
-	{
-		len = 0;
-		if (*(format + i) == '%')
-		{
-			tmp = 0;
-			flags = handle_flags(format + i + 1, &tmp);
-			wid = handle_width(args, format + i + tmp + 1, &tmp);
-			prec = handle_precision(args, format + i + tmp + 1,
-					&tmp);
-			len = handle_length(format + i + tmp + 1, &tmp);
-
-			f = handle_specifiers(format + i + tmp + 1);
-			if (f != NULL)
-			{
-				i += tmp + 1;
-				ret += f(args, output, flags, wid, prec, len);
-				continue;
-			}
-			else if (*(format + i + tmp + 1) == '\0')
-			{
-				ret = -1;
-				break;
-			}
-		}
-		ret += _memcpy(output, (format + i), 1);
-		i += (len != 0) ? 1 : 0;
-	}
-	cleanup(args, output);
-	return (ret);
-}
-
 /**
  * _printf - Outputs a formatted string.
  * @format: Character string to print - may contain directives.
@@ -79,19 +16,55 @@ int run_printf(const char *format, va_list args, buffer_t *output)
  */
 int _printf(const char *format, ...)
 {
-	buffer_t *output;
-	va_list args;
-	int ret;
+    va_list args;
+    va_start(args, format);
 
-	if (format == NULL)
-		return (-1);
-	output = init_buffer();
-	if (output == NULL)
-		return (-1);
+    int num_chars = 0;
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                char ch = va_arg(args, int);
+                write(1, &ch, 1);
+                num_chars++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char*);
+                int len = 0;
+                while (str[len] != '\0')
+                {
+                    len++;
+                }
+                write(1, str, len);
+                num_chars += len;
+            }
+            else if (*format == '%')
+            {
+                write(1, "%", 1);
+                num_chars++;
+            }
+            else
+            {
+                /* Invalid conversion specifier, write an error message */
+                write(2, "Error: Unknown conversion specifier\n", 36);
+                va_end(args);
+                return (-1);
+            }
+        }
+        else
+        {
+            /* Regular character, write it to stdout */
+            write(1, format, 1);
+            num_chars++;
+        }
 
-	va_start(args, format);
+        format++;
+    }
 
-	ret = run_printf(format, args, output);
-
-	return (ret);
+    va_end(args);
+    return (num_chars);
 }
