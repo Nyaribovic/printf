10ccbeddcfab7492d7f8953c4c123fdb8883f1e3
git config --global user.name <nyaribovic@gmail.com>
2023-08-23 00:07:14 -0700

    printf update

--- _printf.c
+++ _printf.c
@@ -1,67 +1,66 @@
-i/*
- * File: _printf.c
- * Auth: Victor Bundi,
- *       Chepngetich Brenda.
- *      
- */
-
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - Custom printf function that handles c, s, and % conversion specifiers.
- * @format: The format string containing the conversion specifiers.
- *
- * Return: The number of characters printed (excluding the null byte used to end output to strings).
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    const char *c;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-    va_start(args, format);
-    for (c = format; *c != '\0'; c++)
-    {
-        if (*c != '%')
-        {
-            putchar(*c);
-            count++;
-        }
-        else
-        {
-            c++;
-            if (*c == 'c')
-            {
-                char ch = va_arg(args, int);
-                putchar(ch);
-                count++;
-            }
-            else if (*c == 's')
-            {
-                char *str = va_arg(args, char *);
-                while (*str != '\0')
-                {
-                    putchar(*str);
-                    str++;
-                    count++;
-                }
-            }
-            else if (*c == '%')
-            {
-                putchar('%');
-                count++;
-            }
-            else
-            {
-                putchar('%');
-                putchar(*c);
-                count += 2;
-            }
-        }
-    }
-    va_end(args);
+	if (format == NULL)
+		return (-1);
 
-    return count;
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
